9a5fc3431f1f61b31aedd71b7848e4ee3125870e
Phong Phí Gia <tpw284@gmail.com>
2021-05-19 16:41:56 +0700

    add apollo client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,35 @@
-import './App.css';
-import Chart from './components/charts';
-import DeviceList from './components/devices';
-import Header from './components/header';
-import Map from './components/map';
-import Statistic from './components/statistic';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "./App.css";
+import Chart from "./components/charts";
+import DeviceList from "./components/devices";
+import Header from "./components/header";
+import Map from "./components/map";
+import Statistic from "./components/statistic";
 
 function App() {
+  const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_URI,
+    cache: new InMemoryCache(),
+  });
+  
   return (
-    <div className="App">
-      <div className="vh-100 mvw-100 m-0 flex-column flex no-wrap">
-        <main className="flex-grow">
-          <Header/>
-        </main>
-        <div className="main-contain d-flex justify-content-between">
-          <Chart/>
-          <div>
-            <Statistic></Statistic>
-            <Map />
+    <ApolloProvider client={client}>
+      <div className="App">
+        <div className="vh-100 mvw-100 m-0 flex-column flex no-wrap">
+          <main className="flex-grow">
+            <Header />
+          </main>
+          <div className="main-contain d-flex justify-content-between">
+            <Chart />
+            <div>
+              <Statistic></Statistic>
+              <Map />
+            </div>
+            <DeviceList />
           </div>
-            <DeviceList/>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
